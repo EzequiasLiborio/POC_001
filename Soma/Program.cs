a5f1a44d1919ed20537f3cebebd1374b74064af2
Ezequias Libório <esl.liborio@gmail.com>
2023-04-09 17:54:04 -0300

    Soma

---
+++ Soma/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Title = "Soma";
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+for (; K < INDICE; )
+{
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+Console.WriteLine(SOMA);
+
