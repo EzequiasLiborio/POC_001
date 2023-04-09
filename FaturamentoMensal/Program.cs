b1d4aac4480803602c5c7b872ba476b893b3d584
Ezequias Libório <esl.liborio@gmail.com>
2023-04-09 18:12:34 -0300

    Inversor de String

---
+++ FaturamentoMensal/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Title = "Percentual de Faturamento por Estado";
+
+double sp = 67836.43;
+double rj = 36678.66;
+double mg = 29229.88;
+double es = 27165.48;
+double outros = 19849.53;
+
+double total = sp + rj + mg + es + outros;
+
+double sp_percentual = (sp / total) * 100;
+double rj_percentual = (rj / total) * 100;
+double mg_percentual = (mg / total) * 100;
+double es_percentual = (es / total) * 100;
+double outros_percentual = (outros / total) * 100;
+
+var qtDecimais = 6;
+
+Console.WriteLine($"Percentual de representação por estado:");
+Console.WriteLine($"SP: {Double.Round(sp_percentual, qtDecimais)}%");
+Console.WriteLine($"RJ: {Double.Round(rj_percentual, qtDecimais)}%");
+Console.WriteLine($"MG: {Double.Round(mg_percentual, qtDecimais)}%");
+Console.WriteLine($"ES: {Double.Round(es_percentual, qtDecimais)}%");
+Console.WriteLine($"Outros: {Double.Round(outros_percentual, qtDecimais)}%");
---
+++ InversorDeString/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Inversor de String";
+
+Console.Write("Digite um texto curto: ");
+string input = Console.ReadLine();
+
+char[] chars = input.ToCharArray();
+int length = chars.Length;
+
+for (int i = 0; i < length / 2; i++)
+{
+    char temp = chars[i];
+    chars[i] = chars[length - i - 1];
+    chars[length - i - 1] = temp;
+}
+
+Console.WriteLine("Texto invertida: {0}", new string(chars));
